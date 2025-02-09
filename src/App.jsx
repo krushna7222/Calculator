b6bd31d04012434532f52a8ddad5fbec4a8ccf3c
Krushna Shahane <142483530+krushna7222@users.noreply.github.com>
2025-02-10 00:45:18 +0530

    bootstrap link and one sample calculator component call

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import "./App.css";
 import Calculator from "./Calculator";
+import NewCalculator from "./NewCalculator";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
     <>
       <Calculator></Calculator>
+      {/* <NewCalculator /> */}
     </>
   );
 }
