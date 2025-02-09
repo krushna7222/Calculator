8ae389195216d901099c4744ccb9918143433771
Krushna Shahane <142483530+krushna7222@users.noreply.github.com>
2025-02-10 00:45:59 +0530

    updated for background color

--- src/Calculator.jsx
+++ src/Calculator.jsx
@@ -42,24 +42,36 @@ const Calculator = () => {
           />
         </div>
         <div className="calculator-buttons">
-          <button onClick={() => setInputValue("")}>AC</button>
-          <button onClick={handleBackspace}>
+          <button className="operand" onClick={() => setInputValue("")}>
+            AC
+          </button>
+          <button className="operand" onClick={handleBackspace}>
             <i className="fa-solid fa-delete-left"></i>
           </button>
-          <button onClick={() => handleButtonClick("/")}>/</button>
-          <button onClick={() => handleButtonClick("*")}>*</button>
+          <button className="operand" onClick={() => handleButtonClick("/")}>
+            /
+          </button>
+          <button className="operand" onClick={() => handleButtonClick("*")}>
+            X
+          </button>
           <button onClick={() => handleButtonClick("7")}>7</button>
           <button onClick={() => handleButtonClick("8")}>8</button>
           <button onClick={() => handleButtonClick("9")}>9</button>
-          <button onClick={() => handleButtonClick("-")}>-</button>
+          <button className="operand" onClick={() => handleButtonClick("-")}>
+            -
+          </button>
           <button onClick={() => handleButtonClick("4")}>4</button>
           <button onClick={() => handleButtonClick("5")}>5</button>
           <button onClick={() => handleButtonClick("6")}>6</button>
-          <button onClick={() => handleButtonClick("+")}>+</button>
+          <button className="operand" onClick={() => handleButtonClick("+")}>
+            +
+          </button>
           <button onClick={() => handleButtonClick("1")}>1</button>
           <button onClick={() => handleButtonClick("2")}>2</button>
           <button onClick={() => handleButtonClick("3")}>3</button>
-          <button onClick={() => handleButtonClick("%")}>%</button>
+          <button className="operand" onClick={() => handleButtonClick("%")}>
+            %
+          </button>
           <button id="ans" onClick={ShowAnswer}>
             =
           </button>
